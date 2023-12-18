06b8c673772a94100dad5854d3762c9c8ebd1e96
imadfen <imadfen20@gmail.com>
2023-12-18 01:07:01 +0100

    setup project

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import Head from "next/head";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Brainy Store</title>
+        <meta name="description" content="This is Brainy Store for swag Brainy products" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
